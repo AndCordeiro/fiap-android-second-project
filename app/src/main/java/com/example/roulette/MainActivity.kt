8573fbce1557ea17db563c5f7f86ea86aea5f49b
Logon Aluno <logonrmlocal@fiap.com.br>
2022-08-06 11:02:47 -0300

    fix: solve warnings

--- app/src/main/java/com/example/roulette/MainActivity.kt
+++ app/src/main/java/com/example/roulette/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.roulette
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,23 +8,24 @@ import android.widget.TextView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Pega referência do button por id
-        val btn_rodar = findViewById(R.id.btn_rodar) as Button
+        val btnRodar = findViewById<Button>(R.id.btn_rodar)
 
         // Ouvinte para click no botão rodar
-        btn_rodar.setOnClickListener {
+        btnRodar.setOnClickListener {
             // Pega referência do textView por id
-            var texto = findViewById<TextView>(R.id.tv_sorteio)
+            val texto = findViewById<TextView>(R.id.tv_sorteio)
 
             // Pega um número inteiro random
-            var numeroSorteado = Random().nextInt(11)
+            val numeroSorteado = Random().nextInt(11)
 
             // Exibe o resultado no textView
-            texto.text = "Número sorteado é: " + numeroSorteado
+            texto.text = "Número sorteado é: $numeroSorteado"
         }
     }
 }
